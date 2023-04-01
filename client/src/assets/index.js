bccb5ba5695e67579c40b3117a7c21028b2d7d4a
Anish <aniish9.18@gmail.com>
2023-04-01 09:51:41 +0530

    First Commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,9 @@
+import downloads from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+
+export {
+  downloads,
+  logo,
+  preview,
+};
---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+import Card from './Card';
+import FormFeild from './FormFeild';
+import Loader from './Loader';
+
+
+export{
+    Card,FormFeild,Loader
+}
