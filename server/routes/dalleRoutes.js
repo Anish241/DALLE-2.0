9c1c2d7e634d729a26d8b37f626532f10fc6b11a
Anish <aniish9.18@gmail.com>
2023-04-01 11:46:45 +0530

    fix 1

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -24,7 +24,7 @@ router.route('/').post(async (req, res) => {
     const { prompt } = req.body;
 
     const aiResponse = await openai.createImage({
-      prompt,
+      prompt: prompt,
       n: 1,
       size: '1024x1024',
       response_format: 'b64_json',
