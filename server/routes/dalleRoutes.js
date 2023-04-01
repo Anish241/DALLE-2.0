a19ff89a0b427d48e23026fe2aa09a747803e998
Anish <aniish9.18@gmail.com>
2023-04-01 11:09:52 +0530

    dalle

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -19,16 +19,15 @@ router.route('/').get((req, res) => {
 router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
-    console.log("image generating...")
+
     const aiResponse = await openai.createImage({
       prompt,
       n: 1,
       size: '1024x1024',
       response_format: 'b64_json',
     });
-    console.log("image generated")
+
     const image = aiResponse.data.data[0].b64_json;
-    
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
